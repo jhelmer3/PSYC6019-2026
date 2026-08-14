764d5da585fd79cddee0f3a2d685a3e18e5ac5cf
Jess Helmer <jhelmer3@gatech.edu>
2026-08-14 11:47:54 -0400

    Update lab_02 slides

--- _targets.R
+++ _targets.R
@@ -9,8 +9,10 @@ tar_option_set(
 tar_source("lab/R")
 
 list(
-  tar_target(lab_01_slides, "lab/slides/lab_01.qmd"),
+  ## index page
+  tar_quarto(lab_index, "lab/lab-index.qmd"),
   
+  ## lab 02
   tar_target(world_cup_player_stats_full, read.csv("lab/raw_data/world_cup_player_stats.csv")),
   tar_target(world_cup_teams_full, read.csv("lab/raw_data/world_cup_teams.csv")),
   tar_target(player_stats, 
@@ -19,5 +21,13 @@ list(
              write.csv(player_stats, "lab/data/world_cup_player_stats.csv",
                        row.names = F),
              format = "file"),
-  tar_quarto(lab_02_slides, "lab/slides/lab_02.qmd")
+  tar_quarto(lab_02_slides, "lab/slides/lab_02.qmd"),
+  tar_quarto(lab_02_activity, "lab/activities/lab_02_activity.qmd"),
+  tar_quarto(lab_02_activity_key, "lab/activities/activity_keys/lab_02_activity_key.qmd"),
+  
+  ## lab 01
+  tar_quarto(lab_01_slides, "lab/slides/lab_01.qmd"),
+  tar_quarto(lab_01_activity, "lab/activities/lab_01_activity.qmd"),
+  tar_quarto(lab_01_activity_key, "lab/activities/activity_keys/lab_01_activity_key.qmd")
+
 )
